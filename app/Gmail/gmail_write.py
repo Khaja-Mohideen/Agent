2d03df84c906c946b37557186996cb8bd8e14b63
Khaja-Mohideen <khajamohideen1104@gmail.com>
2026-09-10 14:16:55 +0530

    Update gmail_write.py

--- app/Gmail/gmail_write.py
+++ app/Gmail/gmail_write.py
@@ -1,1 +1,10 @@
+import os
+import re 
+import urllib.parse 
 
+KEYWORDS = (
+  "gmail", "email", "email", "mail", 
+  "write an email", "send an email", "draft an email",
+  "compose an email", "write email", "send mail", "drafty mail",
+  "compose mail"
+)
